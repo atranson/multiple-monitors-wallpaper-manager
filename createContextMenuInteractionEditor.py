dedee91984be423e5139b8c5a9549ba715874ecf
Vince4x4 <vince4x4@gmail.com>
2015-10-29 18:25:42 +0900

    Adding python script to generate .reg file in order to add desktop contextual menu entry

---
+++ createContextMenuInteractionEditor.py
@@ -0,0 +1,14 @@
+from parameters import workingPath
+import re
+
+output = 'Windows Registry Editor Version 5.00 \n\
+\n\
+[HKEY_CLASSES_ROOT\\Directory\\Background\\shell\\Next Wallpaper]\n\
+@="Next Wallpaper"\n\
+\n\
+[HKEY_CLASSES_ROOT\\Directory\\Background\\shell\\Next Wallpaper\\command]\n\
+@="wscript.exe \\"' + workingPath.replace("\\", "\\\\") + 'ChangeDesktop.vbs\\""'
+
+with open('addContextMenuInteraction.reg', 'w') as file_:
+    file_.write(output)
+    print("addContextMenuInteraction.reg created successfully")
--- parameters.py
+++ parameters.py
@@ -8,6 +8,6 @@ screens = [(1920, 1080), (1920, 1080)] # (width, height)
 if(not(path.endswith("/")) and not(path.endswith("\\"))):
 	path += "/"
 
-workingPath = os.path.dirname(os.path.realpath(__file__)) + "/"
+workingPath = os.path.dirname(os.path.realpath(__file__)) + "\\"
 nbPerWallpaper = len(screens)
 token = "0a5ed3376d044f5c707b0327219547f3e0b46f70" # Do not name a folder of your library with this string
